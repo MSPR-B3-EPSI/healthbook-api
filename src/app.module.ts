1bfdb99acee64b970233c79880548e3aa9cbe6f1
augustin <augustin.coirre@gmail.com>
2026-04-30 14:25:29 +0200

    :sparkles: Add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { RootController } from './controllers/app.controller.js';
 import { UserModule } from './modules/user.module.js';
 import { PostModule } from './modules/post.module.js';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, PostModule],
+  controllers: [RootController],
 })
 export class AppModule {}
--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 
 @Controller()
 export class AppController {}
+
+@Controller()
+export class RootController {
+	@Get()
+	@Redirect('/api', 302)
+	root() {
+		return;
+	}
+}
